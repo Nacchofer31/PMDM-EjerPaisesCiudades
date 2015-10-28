1e0c92bda187f01108d3efb3c6c79f4eeeedd539
Ignacio Ferrer <nachopad31@hotmail.com>
2015-10-28 17:00:46 +0100

    Initial commit

---
+++ app/src/main/java/nach/com/dam2/pmdm_ejerpaisesciudades/MainActivity.java
@@ -0,0 +1,79 @@
+package nach.com.dam2.pmdm_ejerpaisesciudades;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class MainActivity extends Activity {
+
+    Spinner ciudades, paises;
+    ArrayAdapter<String> adap_paises,adap_ciudades;
+    ArrayList<String> ac;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Declaramos los objetos spinner.
+        ciudades = (Spinner) findViewById(R.id.ciudadesSP);
+        paises = (Spinner) findViewById(R.id.paisesSP);
+
+        //Rellenamos nuestro primer spinner
+        String array_paises[]=getResources().getStringArray(R.array.Paises);
+        adap_paises = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,array_paises);
+        paises.setAdapter(adap_paises);
+
+        paises.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                switch (position){
+                    case 0: //Ninguno
+                        break;
+                    case 1: //España
+                        ac=(ArrayList<String>) Arrays.asList(getResources().getStringArray(R.array.ciudades_España));
+                        break;
+                    case 2: //Francia
+                        ac=(ArrayList<String>) Arrays.asList(getResources().getStringArray(R.array.ciudades_Francia));
+                        break;
+                    case 3: //Italia
+                        ac=(ArrayList<String>) Arrays.asList(getResources().getStringArray(R.array.ciudades_Italia));
+                        break;
+                    case 4: //Alemania
+                        ac=(ArrayList<String>) Arrays.asList(getResources().getStringArray(R.array.ciudades_Alemania));
+                }
+                adap_ciudades.addAll(ac);
+                ciudades.setAdapter(adap_ciudades);
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
